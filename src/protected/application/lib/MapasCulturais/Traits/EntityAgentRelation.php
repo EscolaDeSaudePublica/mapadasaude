f8114328ad6a4d45d44fd09673036a06293d5657
Junior-Shyko <franciscoanto@gmail.com>
2021-04-23 15:15:44 -0300

    Adicionando tipo de objeto na classe EntityAgentRelation

--- src/protected/application/lib/MapasCulturais/Traits/EntityAgentRelation.php
+++ src/protected/application/lib/MapasCulturais/Traits/EntityAgentRelation.php
@@ -105,7 +105,8 @@ trait EntityAgentRelation {
                     $groups[] = array(
                         'group'   => $groupEntitie->group,
                         'entitie' => $entitie->name,
-                        'url'     => $entitie->singleUrl
+                        'url'     => $entitie->singleUrl,
+                        'object_type'=> $groupEntitie->owner
                     );
                 }
             }
