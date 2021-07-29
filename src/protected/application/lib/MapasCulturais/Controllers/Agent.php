7ef7ec2e1de9988b8e74d65ef06972c8c8dd37aa
ericsonmoreira <moreira.ericson@gmail.com>
2021-07-29 14:46:49 -0300

    add comentário

--- src/protected/application/lib/MapasCulturais/Controllers/Agent.php
+++ src/protected/application/lib/MapasCulturais/Controllers/Agent.php
@@ -213,16 +213,19 @@ class Agent extends EntityController
         }
     }
 
+    /**
+     * @apiDefine APIPut
+     * @apiDescription Atualiza completamente uma entidade.
+     * @apiParam {Array} [data] Array com valores para popular os atributos da entidade. Use o método describe para descobrir os atributos. Atenção: Todos os dados da entidade devem estar na requisição, inclusive aqueles que não serão alterados.
+     */
     public function PUT_single($data = null)
     {
-
         if (isset($_SESSION['UriHttpReferer'])) {
             $data['newRedirect'] = 'true';
             $data['newRedirectUrl'] = $_SESSION['UriHttpReferer'];
 
             unset($_SESSION['UriHttpReferer']);
         }
-
         parent::PUT_single($data);
     }
 }
