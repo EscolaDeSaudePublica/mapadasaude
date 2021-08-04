503657bdf6fe370c59fb23c67a1399f05221c06c
Junior-Shyko <franciscoanto@gmail.com>
2021-08-04 17:07:38 -0300

    #363 💄 adicionado stilo para o card

--- src/protected/application/lib/modules/EntityOpportunities/layouts/parts/entity-opportunities--item.php
+++ src/protected/application/lib/modules/EntityOpportunities/layouts/parts/entity-opportunities--item.php
@@ -5,26 +5,34 @@ use MapasCulturais\Entities\Opportunity;
 $avatar = $opportunity->avatar ? $opportunity->avatar->transform('avatarSmall') : null;
     
 $url = $this->isEditable() ? $opportunity->editUrl : $opportunity->singleUrl;
+// $entity = $app->repo('ProjectOpportunity')->find();
+
 ?>
-<article class="objeto <?php if($avatar) echo 'has-avatar' ?>">
+<article class="objeto has-avatar" style="background: #E8E8E8;border-radius: 8px;height: 200px;">
     <?php if($avatar): ?>
-    <img src="<?php echo $avatar->url?>">
+    <img src="<?php echo $avatar->url?>" style="background: white; border-radius: 50px; padding: 3px;">
+    <?php else: ?>
+        <img src="http://localhost/assets/img/avatar--opportunity.png" style="background: white; border-radius: 50px; padding: 3px;"/>
     <?php endif; ?>
+    
     <div class="entity-opportunity--content ">
         <a href="<?php echo $url ?>"><?php echo $opportunity->name ?></a>
+        <div class="">
+            <?php $this->part('singles/opportunity-about--registration-dates', ['entity' => $opportunity, 'disable_editable' => true]) ?>
+        </div>
         <?php if($opportunity->status == Opportunity::STATUS_DRAFT): ?>
         <em><?php i::_e('(Rascunho)') ?></em>
         <?php endif; ?>
         <br>
         <div class="objeto-meta">
-            <?php $this->part('singles/opportunity-about--registration-dates', ['entity' => $opportunity, 'disable_editable' => true]) ?>
-        </div>
         <?php if ($app->auth->isUserAuthenticated()): ?>
-        <button class="btn-access" style="float: left; margin-right: 12px;" title="Acessar inscrições">
+            <?php $this->part('singles/opportunity-registrations--form', ['entity' => $opportunity]) ?>
+        <!-- <button class="btn-access" style="float: left; margin-right: 12px;" title="Acessar inscrições">
             <i class="fa fa-arrow-circle-right" aria-hidden="true"></i>
             <a style="color: white;" href="<? echo $url; ?> "> Acessar Inscrição</a>
-        </button>
+        </button> -->
         <?php endif; ?>
-        <hr>
+        </div>
+        
     </div>
 </article>
