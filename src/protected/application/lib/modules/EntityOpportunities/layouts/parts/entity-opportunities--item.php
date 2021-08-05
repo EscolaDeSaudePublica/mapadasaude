e593d23b1aeca6f67676ee1c2883e973e7b9165d
Junior-Shyko <franciscoanto@gmail.com>
2021-08-05 14:16:49 -0300

    #363 📱alterado layout para responsividade

--- src/protected/application/lib/modules/EntityOpportunities/layouts/parts/entity-opportunities--item.php
+++ src/protected/application/lib/modules/EntityOpportunities/layouts/parts/entity-opportunities--item.php
@@ -8,11 +8,11 @@ $url = $this->isEditable() ? $opportunity->editUrl : $opportunity->singleUrl;
 // $entity = $app->repo('ProjectOpportunity')->find();
 
 ?>
-<article class="objeto has-avatar" style="background: #E8E8E8;border-radius: 8px;height: 200px;">
+<article class="objeto has-avatar card-info-opportunity">
     <?php if($avatar): ?>
-    <img src="<?php echo $avatar->url?>" style="background: white; border-radius: 50px; padding: 3px;">
+    <img src="<?php echo $avatar->url?>" class="avatar-card-info-opportunity">
     <?php else: ?>
-        <img src="http://localhost/assets/img/avatar--opportunity.png" style="background: white; border-radius: 50px; padding: 3px;"/>
+        <img src="http://localhost/assets/img/avatar--opportunity.png"  class="avatar-card-info-opportunity"/>
     <?php endif; ?>
     
     <div class="entity-opportunity--content ">
@@ -25,13 +25,13 @@ $url = $this->isEditable() ? $opportunity->editUrl : $opportunity->singleUrl;
         <?php endif; ?>
         <br>
         <div class="objeto-meta">
-        <?php if ($app->auth->isUserAuthenticated()): ?>
+    
             <?php $this->part('singles/opportunity-registrations--form', ['entity' => $opportunity]) ?>
         <!-- <button class="btn-access" style="float: left; margin-right: 12px;" title="Acessar inscrições">
             <i class="fa fa-arrow-circle-right" aria-hidden="true"></i>
             <a style="color: white;" href="<? echo $url; ?> "> Acessar Inscrição</a>
         </button> -->
-        <?php endif; ?>
+       
         </div>
         
     </div>
