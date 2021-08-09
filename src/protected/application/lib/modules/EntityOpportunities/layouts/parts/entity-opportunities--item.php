b03e1909c9c0ffdb7e4b95a7a9e0570f0e387c44
Junior-Shyko <franciscoanto@gmail.com>
2021-08-09 19:14:44 -0300

    #363 - 💄 Ajustando layout do card

--- src/protected/application/lib/modules/EntityOpportunities/layouts/parts/entity-opportunities--item.php
+++ src/protected/application/lib/modules/EntityOpportunities/layouts/parts/entity-opportunities--item.php
@@ -1,6 +1,28 @@
 <?php 
 use MapasCulturais\i;
 use MapasCulturais\Entities\Opportunity;
+/**
+ * Adicionado essas dependências para o funcionamento do componente do angular para buscar o(s) agente(s)
+ */
+$this->bodyProperties['ng-app'] = "entity.app";
+$this->bodyProperties['ng-controller'] = "EntityController";
+
+$this->jsObject['angularAppDependencies'][] = 'entity.module.opportunity';
+$this->jsObject['angularAppDependencies'][] = 'ui.sortable';
+
+$this->addEntityToJs($opportunity);
+
+$this->addOpportunityToJs($opportunity);
+
+$this->addOpportunitySelectFieldsToJs($opportunity);
+
+if($this->isEditable()){
+    $this->addEntityTypesToJs($opportunity);
+    $this->addTaxonoyTermsToJs('tag');
+}
+
+$this->includeAngularEntityAssets($opportunity);
+
 
 $avatar = $opportunity->avatar ? $opportunity->avatar->transform('avatarSmall') : null;
     
@@ -8,14 +30,17 @@ $url = $this->isEditable() ? $opportunity->editUrl : $opportunity->singleUrl;
 // $entity = $app->repo('ProjectOpportunity')->find();
 
 ?>
-<article class="objeto has-avatar card-info-opportunity">
-    <?php if($avatar): ?>
+<article class="objeto card-info-opportunity"  ng-controller="OpportunityController">
+    <div style="width: 100%;">
+        <div style="width: 10%; float:left;">
+        <?php if($avatar): ?>
     <img src="<?php echo $avatar->url?>" class="avatar-card-info-opportunity">
     <?php else: ?>
         <img src="http://localhost/assets/img/avatar--opportunity.png"  class="avatar-card-info-opportunity"/>
     <?php endif; ?>
-    
-    <div class="entity-opportunity--content ">
+        </div>
+        <div style="width: 90%;float:left;">
+        <div class="entity-opportunity--content pad-left-10">
         <a href="<?php echo $url ?>"><?php echo $opportunity->name ?></a>
         <div class="">
             <?php $this->part('singles/opportunity-about--registration-dates', ['entity' => $opportunity, 'disable_editable' => true]) ?>
@@ -25,7 +50,9 @@ $url = $this->isEditable() ? $opportunity->editUrl : $opportunity->singleUrl;
         <?php endif; ?>
         <br>
         <div>
-            <?php $this->part('singles/opportunity-registrations--form', ['entity' => $opportunity]) ?>
+            <?php
+            
+            $this->part('singles/opportunity-registrations--form', ['entity' => $opportunity]) ?>
         <!-- <button class="btn-access" style="float: left; margin-right: 12px;" title="Acessar inscrições">
             <i class="fa fa-arrow-circle-right" aria-hidden="true"></i>
             <a style="color: white;" href="<? echo $url; ?> "> Acessar Inscrição</a>
@@ -34,4 +61,9 @@ $url = $this->isEditable() ? $opportunity->editUrl : $opportunity->singleUrl;
         </div>
         
     </div>
+        </div>
+    </div>
+    
+    
+    
 </article>
