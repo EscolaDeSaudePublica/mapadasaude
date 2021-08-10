41a117d6996a8e2f5790592d80281709cccd468d
Junior-Shyko <franciscoanto@gmail.com>
2021-08-10 11:25:03 -0300

    #162 - 🚚 removendo aba Oportunidade de projetos

--- src/protected/application/lib/modules/EntityOpportunities/layouts/parts/entity-opportunities--item.php
+++ src/protected/application/lib/modules/EntityOpportunities/layouts/parts/entity-opportunities--item.php
@@ -1,6 +1,8 @@
-<?php 
+<?php
+
 use MapasCulturais\i;
 use MapasCulturais\Entities\Opportunity;
+
 /**
  * Adicionado essas dependências para o funcionamento do componente do angular para buscar o(s) agente(s)
  */
@@ -16,7 +18,7 @@ $this->addOpportunityToJs($opportunity);
 
 $this->addOpportunitySelectFieldsToJs($opportunity);
 
-if($this->isEditable()){
+if ($this->isEditable()) {
     $this->addEntityTypesToJs($opportunity);
     $this->addTaxonoyTermsToJs('tag');
 }
@@ -25,45 +27,48 @@ $this->includeAngularEntityAssets($opportunity);
 
 
 $avatar = $opportunity->avatar ? $opportunity->avatar->transform('avatarSmall') : null;
-    
+
 $url = $this->isEditable() ? $opportunity->editUrl : $opportunity->singleUrl;
 // $entity = $app->repo('ProjectOpportunity')->find();
 
 ?>
-<article class="objeto card-info-opportunity"  ng-controller="OpportunityController">
-    <div style="width: 100%;">
-        <div style="width: 10%; float:left;">
-        <?php if($avatar): ?>
-    <img src="<?php echo $avatar->url?>" class="avatar-card-info-opportunity">
-    <?php else: ?>
-        <img src="http://localhost/assets/img/avatar--opportunity.png"  class="avatar-card-info-opportunity"/>
-    <?php endif; ?>
-        </div>
-        <div style="width: 90%;float:left;">
-        <div class="entity-opportunity--content pad-left-10">
-        <a href="<?php echo $url ?>"><?php echo $opportunity->name ?></a>
-        <div class="">
-            <?php $this->part('singles/opportunity-about--registration-dates', ['entity' => $opportunity, 'disable_editable' => true]) ?>
-        </div>
-        <?php if($opportunity->status == Opportunity::STATUS_DRAFT): ?>
-        <em><?php i::_e('(Rascunho)') ?></em>
+<article class="objeto card-info-opportunity" ng-controller="OpportunityController">
+
+    <div class="card-info-header">
+        <?php if ($avatar) : ?>
+            <img src="<?php echo $avatar->url ?>" class="avatar-card-info-opportunity">
+        <?php else : ?>
+            <img src="http://localhost/assets/img/avatar--opportunity.png" class="avatar-card-info-opportunity" />
         <?php endif; ?>
+
+        <span>
+            <a href="<?php echo $url ?>"><?php echo $opportunity->name ?></a>
+            <?php $this->part('singles/opportunity-about--registration-dates', ['entity' => $opportunity, 'disable_editable' => true]) ?>
+        </span>
+    </div>
+    <div class="card-info-content">
+        <?php //if ($opportunity->status == Opportunity::STATUS_DRAFT) : ?>
+            <em><?php //i::_e('(Rascunho)') ?></em>
+        <?php //endif; 
+        $this->part('singles/opportunity-registrations--form', ['entity' => $opportunity]) 
+        ?>
+
+    </div>
+
+    <!-- <div class="entity-opportunity--content pad-left-10">
+       
+        <div class="">
+           
+        </div>
+        <?php //if ($opportunity->status == Opportunity::STATUS_DRAFT) : ?>
+            <em><?php //i::_e('(Rascunho)') ?></em>
+        <?php //endif; ?>
         <br>
         <div>
             <?php
-            
-            $this->part('singles/opportunity-registrations--form', ['entity' => $opportunity]) ?>
-        <!-- <button class="btn-access" style="float: left; margin-right: 12px;" title="Acessar inscrições">
-            <i class="fa fa-arrow-circle-right" aria-hidden="true"></i>
-            <a style="color: white;" href="<? echo $url; ?> "> Acessar Inscrição</a>
-        </button> -->
-       
+            //$this->part('singles/opportunity-registrations--form', ['entity' => $opportunity]) ?>
         </div>
-        
-    </div>
-        </div>
-    </div>
-    
-    
-    
+    </div> -->
+
+
 </article>
