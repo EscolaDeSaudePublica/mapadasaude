ae47264aec447d4afddbb53c93997a92faf24161
Victor Magalhães <victor.magalhaesp@gmail.com>
2021-08-31 09:40:10 -0300

    ajuste session save path docker logs

--- src/protected/application/lib/MapasCulturais/App.php
+++ src/protected/application/lib/MapasCulturais/App.php
@@ -197,9 +197,9 @@ class App extends \Slim\Slim{
             error_reporting(E_ALL ^ E_STRICT);
 
     
-        session_start();
-
         session_save_path(SESSIONS_SAVE_PATH);
+
+        session_start();
         
         if($config['app.offline']){
             $bypass_callable = $config['app.offlineBypassFunction'];
